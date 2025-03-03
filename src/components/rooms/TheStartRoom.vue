987c887715c4a0b2a99dfc1863387b7def1d3dc5
LucaCDRocha <lucadrocha@gmail.com>
2025-03-03 14:37:07 +0100

    Add additional welcome messages and instructions in TheStartRoom

--- src/components/rooms/TheStartRoom.vue
+++ src/components/rooms/TheStartRoom.vue
@@ -12,7 +12,25 @@
 <template>
 	<BaseRoom :position="[0, y, 0]" :size="4" />
 	<!-- Add a ceiling light -->
-	<CeilLantern :position="`0 ${y + 2.15} 0`" :color="lightColor" />	
+	<CeilLantern :position="`0 ${y + 2.15} 0`" :color="lightColor" />
 	<!-- Add a welcome message -->
 	<a-text value="Welcome!" :position="`0 ${y + 2} -1.95`" color="white" align="center"></a-text>
+	<a-text
+		value="Can you escape the room ?"
+		width="3.8"
+		:position="`0 ${y + 1.8} -1.95`"
+		color="white"
+		align="center"></a-text>
+	<a-text
+		value="You can interact with some objects with the tip of the wand in vr or by clicking on them on pc"
+		width="3.8"
+		:position="`0 ${y + 1.4} -1.95`"
+		color="white"
+		align="center"></a-text>
+	<a-text
+		value="Take the wand to start"
+		width="3.8"
+		:position="`0 ${y + 1} -1.95`"
+		color="white"
+		align="center"></a-text>
 </template>
